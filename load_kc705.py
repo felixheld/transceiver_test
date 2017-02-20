9b2a273e7f976505222494047a0a9b76f5f736e9
Florent Kermarrec <florent@enjoy-digital.fr>
2017-02-20 17:37:52 +0100

    use target parameter to select kc705 or kcu105 programmer

--- load_kc705.py
+++ load_kc705.py
@@ -2,4 +2,6 @@
 from litex.build.xilinx import VivadoProgrammer
 
 prog = VivadoProgrammer()
-prog.load_bitstream("build_kc705/gateware/top.bit")
+prog.load_bitstream(
+	bitstream_file="build_kc705/gateware/top.bit",
+	target="localhost:3121/xilinx_tcf/Digilent/210203336949A")
--- load_kcu105.py
+++ load_kcu105.py
@@ -2,4 +2,6 @@
 from litex.build.xilinx import VivadoProgrammer
 
 prog = VivadoProgrammer()
-prog.load_bitstream("build_kcu105/gateware/top.bit")
+prog.load_bitstream(
+	bitstream_file="build_kcu105/gateware/top.bit",
+	target="localhost:3121/xilinx_tcf/Digilent/210308A0E6EA")
