4d5ac0d30becd24a974619538d778f35e7a3e579
Florent Kermarrec <florent@enjoy-digital.fr>
2018-05-28 18:14:19 +0200

    pcie_cpri: keep up to date with litescope

--- pcie_cpri.py
+++ pcie_cpri.py
@@ -176,7 +176,7 @@ class GTPTestSoC(BaseSoC):
                 gtp.decoders[1].d,
                 gtp.decoders[1].k,
             ]
-            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 256, cd_ratio=2, cd="tx")
+            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 256, cd="tx")
 
     def do_exit(self, vns):
         if hasattr(self, "analyzer"):
